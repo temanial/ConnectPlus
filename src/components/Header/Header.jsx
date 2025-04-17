d691612d33856f018780bfbf1020b9ee851c7373
Temanial <vadim.starostin.07@gmail.com>
2025-04-17 15:06:09 +0700

    useLocation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
+  const location = useLocation();
   return (
     <div>
       <header>
@@ -18,10 +19,14 @@ export default function Header() {
           </div>
           <div className="header-pages" id="nav-menu">
             <ul className="header-menu">
-              <li><Link to="/TradeIn">Trade-in</Link></li>
+              {location.pathname !== '/TradeIn' && (
+                <li><Link to="/TradeIn">Trade-in</Link></li>
+              )}
               <li><Link to="/smartphones">Смартфоны</Link></li>
               <li><Link to="/tariffs">Тарифные планы</Link></li>
-              <li><Link to="/ServiceCenter">Сервис</Link></li>
+              {location.pathname !== '/ServiceCenter' && (
+                <li><Link to="/ServiceCenter">Сервис</Link></li>
+              )}
               <li><Link className="user" to="/login">Личный кабинет</Link></li>
             </ul>
           </div>
